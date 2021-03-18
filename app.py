718f95cc64dab3019ff13e2a09f6f8afb6e182de
Mayuri Ishii <emisahima321@icloud.com>
2021-03-18 14:11:17 +0900

    データベースのテーブル作りました的な？

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask , render_template , request , redirect , session
+
+app = Flask(__name__)
+
+app.secret_key = 'sunabakoza'
+
+@app.route('/')
+def toppage():
+    return render_template('toppage.html')
+
+@app.route('/favorite')
+def favorite():
+    return render_template('favorite.html')
+
+if __name__ == "__main__":
+    app.run()
---
+++ static/js/common.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.drawer').drawer();
+  });
+  
+
+<a href="javascript:history.go(-1)">[戻る]</a>
