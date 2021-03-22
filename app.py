8284a1c712d5507f20a5346ace3db89c13279802
izumi nango <izumi.in.perth@gmail.com>
2021-03-22 15:30:49 +0900

    login-account追加しました

--- app.py
+++ app.py
@@ -12,5 +12,18 @@ def toppage():
 def favorite():
     return render_template('favorite.html')
 
+@app.route('/my_login')
+def my_login():
+    return render_template('my_login.html')
+
+@app.route('/my_account')
+def my_account():
+    return render_template('my_account.html')
+
+@app.route('/my_user')
+def my_user():
+    return render_template('my_user.html')    
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run()    
